b588ad3d97f70e15cd6e490af7211e0a54c5188e
SkyDiamond <anan_vee@hotmail.com>
2018-04-27 20:22:20 +0700

    client now can delete bear

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,12 +1,19 @@
 import axios from 'axios'
 
-export const fetchBear = function() {
+export function fetchBear(){
   return (dispatch) => {
     axios.get('http://localhost:8000/api/bears')
-    .then( (response) => {
-      dispatch({type:"FETCH_BEAR", payload:response.data});
+    .then(result => {
+      dispatch({type: 'FETCH_BEAR', payload: result.data})
+    })
+  }
+}
+
+export function deleteBear(id) {
+  return (dispatch) => {
+      axios.delete(`http://localhost:8000/api/bears/${id}`)
+      .then(() => {
+          dispatch(fetchBear())
       })
-      .catch( (error) => {
-      });
   }
 }
